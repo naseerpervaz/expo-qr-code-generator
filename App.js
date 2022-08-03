f575ccec4a352086512efdadaeae46848605a592
Chelsea Farley <chelsea@tripwiretech.com>
2022-08-03 19:19:28 +1200

    qr code generator

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import QRCode from 'react-native-qrcode-svg';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <QRCode value='https://www.github.com/chelseafarley' />
       <StatusBar style="auto" />
     </View>
   );
